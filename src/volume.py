08cae9b4331853359ff603af978342d5d3fede11
avihaigiuili <avihai@sodyo.com>
2025-11-10 19:56:17 +0200

    add comments to volume rendering script

--- src/volume.py
+++ src/volume.py
@@ -13,6 +13,8 @@ def volume_render(rgb, sigma, z_vals, rays_d, white_bkgd=True):
       acc:      (N_rays, 1) accumulated opacity
     """
     device = rgb.device
+
+    # Compute deltas (distances between samples) "𝛿_𝑖 in the paper" ​: is the spacing between consecutive samples (scaled by ∥ ray_d ∥).
     deltas = z_vals[..., 1:] - z_vals[..., :-1]                         # (N_rays, N_samples-1)
     # infinity delta for the last sample
     delta_inf = 1e10 * torch.ones_like(deltas[..., :1])
@@ -21,18 +23,21 @@ def volume_render(rgb, sigma, z_vals, rays_d, white_bkgd=True):
     deltas = deltas * torch.linalg.norm(rays_d[:, None, :], dim=-1)
 
     # Volumetric rendering
+    """Convert densities to alphas"""
     alpha = 1. - torch.exp(-sigma.squeeze(-1) * deltas)                 # (N_rays, N_samples)
-    # Accumulated transmittance T
+    # Accumulated transmittance T (T_i : prob to reach sample i without termination)
     # cumprod_exclusive: prepend 1 then cumprod without last element
     eps = 1e-10
     accum_prod = torch.cumprod(1. - alpha + eps, dim=-1)
     trans = torch.cat([torch.ones_like(accum_prod[..., :1]), accum_prod[..., :-1]], dim=-1)
 
     weights = alpha * trans                                             # (N_rays, N_samples)
+    # Composite color and depth
     comp_rgb = torch.sum(weights[..., None] * rgb, dim=-2)              # (N_rays, 3)
     depth = torch.sum(weights * z_vals, dim=-1, keepdim=True)           # (N_rays, 1)
     acc = torch.sum(weights, dim=-1, keepdim=True)                      # (N_rays, 1)
 
+    # We also add a white background term (1−∑w_i) to the RGB if white_bkgd=True
     if white_bkgd:
         comp_rgb = comp_rgb + (1. - acc)  # white background
 
